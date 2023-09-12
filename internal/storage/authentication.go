33cfde1395c5f5d221180bc41ebaa1434dabc057
Ivan Cherepanov <v.he2014@gmail.com>
2023-09-12 20:19:03 +0300

    update storage

--- internal/storage/authentication.go
+++ internal/storage/authentication.go
@@ -1,5 +1,11 @@
 package storage
 
-type Authentication struct {
-	storage *Storage
+import "authentication-service/internal/model"
+
+func (s *Storage) GetToken(authentication *model.Token) error {
+	return nil
+}
+
+func (s *Storage) RefreshToken(authentication *model.Token) error {
+	return nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/mongo"
 
 type Storage struct {
 	db                    *mongo.Database
-	AuthenticationStorage *AuthenticationStorage
+	authenticationStorage *AuthenticationStorage
 }
 
 func New(db *mongo.Database) *Storage {
