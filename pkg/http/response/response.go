3bcab5702c7ba14e0dcaf1984938478b4b583ef4
Ivan Cherepanov <v.he2014@gmail.com>
2023-09-17 17:38:43 +0300

    add http/response

---
+++ pkg/http/response/response.go
@@ -0,0 +1,26 @@
+package response
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
+// Error fills the response body with an error and redirects the data to the responder
+func Error(w http.ResponseWriter, code int, err error) {
+	if errors.Is(err, errors.New("unexpected server error")) {
+		code = http.StatusInternalServerError
+	}
+	Respond(w, code, map[string]string{"error": err.Error()})
+}
+
+// Respond returns a response to a client request in the form of status code and response body
+func Respond(w http.ResponseWriter, code int, data interface{}) {
+	// return code status to client
+	w.WriteHeader(code)
+
+	// body return to client if not nil
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
